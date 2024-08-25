bcb143baa794cb72376d8010f6c52aea4f5e2219
Warren Kim <89424559+warrenjkim@users.noreply.github.com>
2024-08-24 22:30:00 -0700

    Add key value node to visitor

--- include/visitors/visitor.h
+++ include/visitors/visitor.h
@@ -8,16 +8,18 @@ class BooleanNode;
 class NullNode;
 class NumberNode;
 class ObjectNode;
+class KeyValueNode;
 class StringNode;
 
 class JsonVisitor {
  public:
-  void visit(const ArrayNode& node);
-  void visit(const BooleanNode& node);
-  void visit(const NullNode& node);
-  void visit(const NumberNode& node);
-  void visit(const ObjectNode& node);
-  void visit(const StringNode& node);
+  virtual void visit(const ArrayNode& node) = 0;
+  virtual void visit(const BooleanNode& node) = 0;
+  virtual void visit(const NullNode& node) = 0;
+  virtual void visit(const NumberNode& node) = 0;
+  virtual void visit(const ObjectNode& node) = 0;
+  virtual void visit(const KeyValueNode& node) = 0;
+  virtual void visit(const StringNode& node) = 0;
 };
 
 }  // namespace json
