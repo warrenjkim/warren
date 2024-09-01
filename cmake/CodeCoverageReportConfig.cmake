33eb7c0baf7e5cca7bba88c4f300e1b2349185ad
Warren Kim <89424559+warrenjkim@users.noreply.github.com>
2024-09-01 11:53:02 -0700

    Cmake config update
    * force no copy elision optimization
    * force O0 optimization

--- cmake/CodeCoverageReportConfig.cmake
+++ cmake/CodeCoverageReportConfig.cmake
@@ -1,8 +1,6 @@
 include(CMakeParseArguments)
-
 # Find gcovr
 find_program(GCOVR_PATH gcovr)
-
 # Defines a target for reporting test code coverage information.
 # Builds dependencies, runs tests, and generates a report.
 #
@@ -20,35 +18,19 @@ function(generate_coverage_report)
     # Parse arguments
     set(multiValueArgs TARGETS TESTS)
     cmake_parse_arguments(COVERAGE "" "" "${multiValueArgs}" ${ARGN})
-
     set(COVERAGE_REPORT_DIR "${CMAKE_BINARY_DIR}/report")
     set(COVERAGE_DATA_STAMP_FILE "${CMAKE_BINARY_DIR}/coverage_data.stamp")
     set(COVERAGE_REPORT_FILE "${COVERAGE_REPORT_DIR}/index.html")
-
     if(NOT GCOVR_PATH)
         message(FATAL_ERROR "gcovr not found! Aborting...")
     endif()
-
     string(TOLOWER "${CMAKE_BUILD_TYPE}" LOWER_CMAKE_BUILD_TYPE)
     if("${LOWER_CMAKE_BUILD_TYPE}" STREQUAL "coverage")
         message(STATUS "Coverage will be measured for targets: ${COVERAGE_TARGETS}")
-
         # Create Coverage build type
         set(CMAKE_CXX_FLAGS_COVERAGE "${CMAKE_CXX_FLAGS_DEBUG}" PARENT_SCOPE)
         set(CMAKE_C_FLAGS_COVERAGE "${CMAKE_C_FLAGS_DEBUG}" PARENT_SCOPE)
         set(CMAKE_CONFIGURATION_TYPES "${CMAKE_CONFIGURATION_TYPES}" Coverage PARENT_SCOPE)
-
-        # Add compile/link flags to targets
-        foreach(T ${COVERAGE_TARGETS})
-            set_property(TARGET ${T} APPEND_STRING PROPERTY COMPILE_FLAGS " -g --coverage")
-            set_property(TARGET ${T} APPEND_STRING PROPERTY LINK_FLAGS " -g --coverage")
-        endforeach()
-
-        # Tests are not compiled with coverage info, since we do not want them
-        # included in the report
-        foreach(T ${COVERAGE_TESTS})
-            set_property(TARGET ${T} APPEND_STRING PROPERTY LINK_FLAGS " -g --coverage")
-        endforeach()
 
         # Use coverage_data.stamp as a dependency proxy for coverage data files
         add_custom_command(OUTPUT ${COVERAGE_DATA_STAMP_FILE}
@@ -59,8 +41,7 @@ function(generate_coverage_report)
             COMMENT "Running tests to generate coverage data"
             VERBATIM
         )
-
-        # Generage the HTML report, dependent on coverage data
+        # Generate the HTML report, dependent on coverage data
         add_custom_command(OUTPUT ${COVERAGE_REPORT_FILE}
             COMMAND ${CMAKE_COMMAND} -E remove_directory "${COVERAGE_REPORT_DIR}"
             COMMAND ${CMAKE_COMMAND} -E make_directory "${COVERAGE_REPORT_DIR}"
@@ -68,11 +49,14 @@ function(generate_coverage_report)
             -r "${CMAKE_SOURCE_DIR}"
             --object-directory "${CMAKE_BINARY_DIR}"
             -o "${COVERAGE_REPORT_FILE}"
+            --exclude-directories ".*googletest.*"
+            --exclude ".*_test\.cc"
+            --exclude ".*gtest.*"
+            --exclude ".*gmock.*"
             DEPENDS ${COVERAGE_DATA_STAMP_FILE}
             COMMENT "Generating coverage report"
             VERBATIM)
         add_custom_target(coverage DEPENDS ${COVERAGE_REPORT_FILE})
-
         # Clean up the report directory on make clean
         set_property(DIRECTORY APPEND PROPERTY ADDITIONAL_MAKE_CLEAN_FILES ${COVERAGE_REPORT_DIR})
     endif()
