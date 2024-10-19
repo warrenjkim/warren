c42f34779e3f3624121102234a5b84cb303d814b
Warren Kim <89424559+warrenjkim@users.noreply.github.com>
2024-10-19 12:09:19 -0700

    Add as() to force cast to the impl type in Type (#27)
    * This breaks it being an interface but that is okay since this is only
      one level of inheritance.

--- include/types/type.h
+++ include/types/type.h
@@ -9,6 +9,17 @@ class Type {
   virtual void accept(Visitor& visitor) const = 0;
 
  public:
+  template <typename T>
+  T* as() {
+    return dynamic_cast<T*>(this);
+  }
+
+  template <typename T>
+  const T* as() const {
+    return dynamic_cast<const T*>(this);
+  }
+
+ public:
   const bool operator==(const Type& other) const;
   const bool operator!=(const Type& other) const;
 
