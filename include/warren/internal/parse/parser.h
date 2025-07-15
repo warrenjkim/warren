67ef215ca61d888bdcd49bd9832bc1172b5f7641
Warren Kim <89424559+warrenjkim@users.noreply.github.com>
2025-07-14 22:17:33 -0700

    [cleanup] Trim codebase (#124)
    * Finishes the parser migration.
    * Removes the unused utils file.

--- include/warren/internal/parse/parser.h
+++ include/warren/internal/parse/parser.h
@@ -1,9 +1,5 @@
 #pragma once
 
-#include <optional>
-#include <string_view>
-
-#include "warren/internal/dsa/queue.h"
 #include "warren/internal/nodes/array.h"
 #include "warren/internal/nodes/boolean.h"
 #include "warren/internal/nodes/node.h"
@@ -12,50 +8,6 @@
 #include "warren/internal/nodes/object.h"
 #include "warren/internal/nodes/string.h"
 #include "warren/internal/parse/lexer.h"
-
-namespace json {
-
-struct Token;
-
-class Parser {
- public:
-  static nodes::Node* parse(const std::string_view json);
-  static nodes::Node* parse(dsa::Queue<Token>& tokens);
-
- private:
-  static nodes::Object* parse_object(dsa::Queue<Token>& tokens,
-                                     const size_t indent_level = 0);
-  static nodes::Array* parse_array(dsa::Queue<Token>& tokens,
-                                   const size_t indent_level = 0);
-
- private:
-  static nodes::Node* parse_value(dsa::Queue<Token>& tokens,
-                                  const size_t indent_level = 0);
-  static nodes::String* parse_string(dsa::Queue<Token>& tokens,
-                                     const size_t indent_level = 0);
-  static nodes::Number* parse_number(dsa::Queue<Token>& tokens,
-                                     const size_t indent_level = 0);
-  static std::optional<dsa::Pair<std::string, nodes::Node*>> parse_key_value(
-      dsa::Queue<Token>& tokens, const size_t indent_level = 0);
-  static nodes::Boolean* parse_boolean(dsa::Queue<Token>& tokens,
-                                       const size_t indent_level = 0);
-  static nodes::Null* parse_null(dsa::Queue<Token>& tokens,
-                                 const size_t indent_level = 0);
-
- private:
-  static const bool expect_next(dsa::Queue<Token>& tokens,
-                                const Token& expected);
-
- private:
-  Parser() = delete;
-  ~Parser() = delete;
-  Parser(Parser&&) = delete;
-  Parser(const Parser&) = delete;
-  Parser& operator=(Parser&&) = delete;
-  Parser& operator=(const Parser&) = delete;
-};
-
-}  // namespace json
 
 namespace json {
 
