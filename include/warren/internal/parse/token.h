df1c55a208bb2d6d53604d0c7a7917ac8345807f
Warren Kim <89424559+warrenjkim@users.noreply.github.com>
2025-08-18 22:33:47 -0700

    [cleanup] Remove dead code (#127)
    * Remove old token struct

--- include/warren/internal/parse/token.h
+++ include/warren/internal/parse/token.h
@@ -4,37 +4,6 @@
 
 namespace json {
 
-// TODO(DELETE ONCE MIGRATED OUT OF OLD PARSING LOGIC)
-enum class TokenType {
-  OBJECT_START,
-  OBJECT_END,
-  ARRAY_START,
-  ARRAY_END,
-  QUOTE,
-  COMMA,
-  COLON,
-  STRING,
-  NUMBER,
-  BOOLEAN,
-  JSON_NULL,
-  UNKNOWN,
-  END_OF_JSON
-};
-
-struct Token {
-  std::string value;
-  TokenType type;
-
-  Token(std::string value = "", TokenType type = TokenType::UNKNOWN)
-      : type(type), value(std::move(value)) {}
-  Token(char value = '\0', TokenType type = TokenType::UNKNOWN)
-      : type(type), value(1, value) {}
-
-  bool operator==(const Token& other) const {
-    return value == other.value && type == other.type;
-  }
-};
-
 namespace syntax {
 
 enum class TokenType {
